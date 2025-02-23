8b43fc21e95dcf11234d4728caae8021c25ed0c2
Shubham Shekhar <imshubham224@gmail.com>
2025-02-24 01:46:53 +0530

    Commit

---
+++ src/main/java/org/selenium/utilities/BaseTest.java
@@ -0,0 +1,34 @@
+package org.selenium.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+public class BaseTest {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+        // Navigate to Naukri URL from config file
+        driver.get(ConfigReader.getProperty("url"));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
---
+++ src/test/java/Naukri.java
@@ -0,0 +1,23 @@
+import org.selenium.pages.NaukriPage;
+import org.selenium.utilities.BaseTest;
+import org.selenium.utilities.ConfigReader;
+import org.testng.annotations.Test;
+
+
+public class Naukri extends BaseTest {
+
+    @Test
+    public void testNaukriAutomation() {
+        NaukriPage naukriPage = new NaukriPage(driver);
+        // Get username & password from config
+        String username = ConfigReader.getProperty("username");
+        String password = ConfigReader.getProperty("password");
+        String location = ConfigReader.getProperty("location");
+        naukriPage.clickOnLogin();
+        naukriPage.login(username, password);
+        naukriPage.viewProfile();
+//        naukriPage.updateLocation(location);
+
+        System.out.println("Profile updated successfully!");
+    }
+}
